f154359366cfab8e557c32a3e7eea85dfc17a596
E.Rodionov <krowstel@gmail.com>
2018-04-25 23:40:13 +0300

    created view

--- index.php
+++ index.php
@@ -7,3 +7,32 @@ $game = new Game();
 
 $game->fillMap();
 $game->showMap();
+
+$map = $game->map;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>WarSheeps</title>
+  <link rel="stylesheet" type="text/css" href="site.css">
+</head>
+<body>
+  <?=$game->showMap() ?>
+  <div class="wrapper">
+    <div class="field">
+      <?php foreach($map as $line):?>
+        <?php foreach($line as $cell):?>
+          <div class="cell">
+          <?php if($cell): ?>
+          <img src="sheep.png" class="sheep-icon" >
+          <?php endif ?>
+          </div>
+        <?php endforeach?>
+      <?php endforeach?>
+    </div>
+  </div>
+</body>
+</html>
